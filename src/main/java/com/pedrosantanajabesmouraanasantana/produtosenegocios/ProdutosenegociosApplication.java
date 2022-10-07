5de97b5514a135f9247387e4959afe082c8fc8de
AnaSantana <anapedra.mil@gmail.com>
2022-10-07 10:13:04 -0300

    Initial commit

---
+++ src/main/java/com/pedrosantanajabesmouraanasantana/produtosenegocios/ProdutosenegociosApplication.java
@@ -0,0 +1,13 @@
+package com.pedrosantanajabesmouraanasantana.produtosenegocios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosenegociosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdutosenegociosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedrosantanajabesmouraanasantana/produtosenegocios/ProdutosenegociosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrosantanajabesmouraanasantana.produtosenegocios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosenegociosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
